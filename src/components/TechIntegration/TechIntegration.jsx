3b81e8c30fb0803b466a538d904dd26a144a9f06
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2023-09-22 22:46:30 +0530

    ft. responsive changes

--- src/components/TechIntegration/TechIntegration.jsx
+++ src/components/TechIntegration/TechIntegration.jsx
@@ -6,7 +6,7 @@ import integrationIllus from "../../assets/illustrations/techIntegration.svg";
 
 function TechIntegration() {
   return (
-    <div className="bg-white w-full sm:grid sm:grid-cols-2  md:px-24 py-12">
+    <div className="bg-white w-full sm:grid sm:grid-cols-2 px-8 md:px-24 py-12">
       <img src={integrationIllus} alt="tech intrgation" />
       <div className="flex flex-col items-center w-full gap-4">
         <h1 className="text-5xl mb-4">
@@ -16,8 +16,8 @@ function TechIntegration() {
         </h1>
         {techIntegrationContent.map((item) => {
           return (
-            <div className="w-4/5 flex gap-4 bg-white rounded-md  border border-indigo-800 p-8 shadow-xl transition hover:border-indigo-500/90 hover:shadow-indigo-500/20">
-              <item.icon className="h-10 w-10 text-white bg-indigo-500 rounded-full p-2" />
+            <div className="w-full md:w-4/5 flex gap-4 bg-white rounded-md  border border-indigo-800 p-8 shadow-xl transition hover:border-indigo-500/90 hover:shadow-indigo-500/20">
+              <item.icon className="h-10 w-12 md:w-10 text-white bg-indigo-500 rounded-full p-2" />
               <div className="flex flex-col gap-2">
                 <h2 className="text-lg font-bold text-gray-900 md:text-xl">
                   {item.title}
